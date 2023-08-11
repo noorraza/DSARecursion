d394cbdc4f4eff0f80673778494b1d07a4792b7b
Noor <noor.raza1998@gmail.com>
2023-08-11 19:36:37 -0400

    Completed ques7

---
+++ PrintZigzag.java
@@ -0,0 +1,41 @@
+/**
+ * 1. Here are a few sets of inputs and outputs for your reference
+ * Input1 -> 1
+ * Output1 -> 1 1 1
+ *
+ * Input2 -> 2
+ * Output2 -> 2 1 1 1 2 1 1 1 2
+ *
+ * Input2 -> 3
+ * Output3 -> 3 2 1 1 1 2 1 1 1 2 3 2 1 1 1 2 1 1 1 2 3
+ *
+ * 2. Figure out the pattern and complete the recursive function pzz to achieve the above for any positive number n.
+ */
+
+
+import java.util.*;
+
+
+
+public class PrintZigzag {
+
+    public static void main(String[] args) throws Exception {
+        // write your code here
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        pzz(n);
+    }
+
+    public static void pzz(int n){
+        if(n==0){
+            return;
+        }
+        System.out.print(n);
+        pzz(n-1);
+        System.out.print(n);
+        pzz(n-1);
+        System.out.print(n);
+        
+    }
+
+}
